d356ba45177dbc737c302f0ee82791fadf38d01e
Zia Rauwolf <toolgirl-github@tentacle.net>
2019-01-19 22:24:07 -0800

    Add soundloader.

--- testapp/main.py
+++ testapp/main.py
@@ -1,8 +1,17 @@
+import sys
+
 from kivy.app import App
+from kivy.core.audio import SoundLoader
 from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+
 
 class MainWindow(Widget):
-    pass
+    filename = StringProperty(sys.argv[1])
+
+    def play_sound(self):
+        sound = SoundLoader.load(self.filename)
+        sound.play()
 
 class TestApp(App):
     def build(self):
